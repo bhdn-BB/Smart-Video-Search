3dee4ad5c0fde9d72f16936215d761afd368d4b8
Bohdan Boklah <b.boklag@gmail.com>
2025-09-06 19:06:14 +0300

    add config for ffmpeg

--- backend/global_config.py
+++ backend/global_config.py
@@ -8,16 +8,20 @@ TARGET_FORMAT_NOTE = '360p'
 MS_IN_SECOND = 1000
 
 QUALITY_INTERVAL_MAP = {
-    10: 1.0,
-    9: 1.5,
-    8: 2.0,
-    7: 2.5,
-    6: 3.0,
-    5: 4.0,
-    4: 5.0,
-    3: 6.0,
-    2: 8.0,
-    1: 10.0,
+    10: "fps=1/1.0",
+    9:  "fps=1/1.5",
+    8:  "fps=1/2.0",
+    7:  "fps=1/2.5",
+    6:  "fps=1/3.0",
+    5:  "fps=1/4.0",
+    4:  "fps=1/5.0",
+    3:  "fps=1/6.0",
+    2:  "fps=1/8.0",
+    1:  "fps=1/10.0",
 }
 
-QUALITY_FRAMES = 15
+QUALITY_FRAMES = 31
+
+ENCODING_SERVICE_URL = "http://localhost:8001/encode"
+
+MAX_LENGTH_QUERY = 150
