577385e349c4ceb603fe0635da2db6d2478515d0
Bohdan Boklah <b.boklag@gmail.com>
2025-09-06 19:05:49 +0300

    init query_request dto

--- backend/dto/query_request.py
+++ backend/dto/query_request.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
+from backend.global_config import MAX_LENGTH_QUERY
 
 
 class QueryRequest(BaseModel):
-    query: str = Field(..., max_length=150)
+    query: str = Field(..., max_length=MAX_LENGTH_QUERY)
