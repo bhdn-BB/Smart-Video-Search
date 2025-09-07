b39a34d5c1170dd3681052c5ea3ca9b6269bdceb
Bohdan Boklah <b.boklag@gmail.com>
2025-09-07 17:31:00 +0300

    init text encoder

--- backend/routers/text_encoding.py
+++ backend/routers/text_encoding.py
@@ -1,23 +1,12 @@
 from fastapi import APIRouter
-import aiohttp
 from backend.dto.query_request import QueryRequest
 from backend.dto.query_response import QueryResponse
-from backend.global_config import ENCODING_SERVICE_URL
+from backend.services.grpc.client import EncodingClient
 
-router = APIRouter(
-    prefix="/text_encoding",
-    tags=["Text encoding"]
-)
+router = APIRouter(prefix="/text_encoding", tags=["Text encoding"])
+client = EncodingClient()
 
 @router.post("/send_prompt", response_model=QueryResponse)
 async def send_prompt(request: QueryRequest):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            ENCODING_SERVICE_URL,
-            json={"prompt": request.query}
-        ) as resp:
-            text_embedding = await resp.json()
-    return {
-        "status": "ok",
-        "query_embedding": text_embedding
-    }
+    response = client.encode_text(request.query)
+    return QueryResponse(embedding=list(response.embedding))
